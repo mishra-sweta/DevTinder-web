3d435d9b15c05409f61141d08d3079d3e2c114de
mishra-sweta <swetamishra.liluah@gmail.com>
2025-05-14 17:55:23 +0530

    Rotinng,navbar, footer

---
+++ src/components/Body.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+
+const Body = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
